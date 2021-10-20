4f050d6d061e08444c3506b2e37f9878260e305b
yujijun <jijunyu140@gmail.com>
2021-10-20 11:17:45 +0800

    Update complete AIfigure.R

--- script/04_AIfigure.R
+++ script/04_AIfigure.R
@@ -1,3 +1,4 @@
+##------ Update Time: Wed Oct 20 11:14:46 2021 ------##
 ############ load all package ##### 
 setwd("/mnt/data/yjj/BrainImmune")
 library(SingleR)
@@ -24,13 +25,7 @@ extrafont::loadfonts()
 ############ Hyperparameter #### 
 base_path <- "/mnt/data/yjj/BrainImmune/"
 setwd(base_path)
-output_path <- "./output/figure/"
-cluster_color.v2 <- c("#0a62ae","#139277","#91cbbb","#7d91bc",
-                      "#de7f07","#ba2d1b","#fdde86","#6791a7",
-                      "#7d8aaf","#48b1d3","#e43983","#ef9572",
-                      "#dd3e23","#8c741a","#99a2c0","#0a60ad",
-                      "#1169b4","#4d4d4a","#6f68a7","f09f7c")
-
+output_fpath <- "./output/figure/"
 anno.final.color <- c(
   "#ba2d1b","#1169b4","#91cbbb","#139277","#6B8E23",
   "#48b1d3","#7d91bc","#4d4d4a","#de7f06","#BC8F8F",
@@ -49,7 +44,7 @@ width = 178
 load("/mnt/data/yjj/BrainImmune/output/data/r1_03_control.seuobj.singlet.RData")
 load("/mnt/data/yjj/BrainImmune/output/data/r4_control.seuobj.singlet.lastmeta.RData")
 control.seuobj.singlet@meta.data <- control.seuobj.singlet.lastmeta
-
+load("/mnt/data/yjj/BrainImmune/output/data/r4_Bcell.RData")
 ############ AI figure ######
 #### 01_Dimplot for sum.v2 ####
 library(RColorBrewer)
@@ -59,15 +54,15 @@ plot1.2 <- DimPlot(control.seuobj.singlet,label = T,repel = T,reduction = "umap"
 ggsave(filename = paste0(output_fpath,prefix,"1_Dimplot_sum.v2.pdf"),plot = plot1.2,
        device = "pdf",height = height,width = width+20,units = "mm")
 
-#### Dimplot for only stemcell ####
+#### 02_Dimplot for only stemcell ####
 Idents(control.seuobj.singlet) <- control.seuobj.singlet$sum.v2
 plot1.3 <- DimPlot(control.seuobj.singlet,repel = F,reduction = "umap",
                    label.size = 5,cols = anno.stemcell,pt.size = 0.3) +
   mytheme + NoLegend()
-ggsave(filename = paste0(output_path,prefix,"2_Dimplot_Stemcell.pdf"),plot = plot1.3,
+ggsave(filename = paste0(output_fpath,prefix,"2_Dimplot_Stemcell.pdf"),plot = plot1.3,
        device = "pdf",height = height/3,width = width/3,units = "mm")
 
-#### Dotplot ####
+#### 03_Dotplot for all clusters ####
 Allcellfeatures <- c("Kit","Cd34","Ifitm1","Cd79a","Cd79b","Cd3g","Cd8b1","Cd4",
                      "Il7r","Nkg7","Klrd1","Siglech",
                      "Gata3","S100a8","S100a9","Camp","Ngp",
@@ -82,10 +77,10 @@ plot2 <- DotPlot(control.seuobj.singlet,features = Allcellfeatures,
                  cols = c("lightgrey", "#FF9900")) +
   theme(axis.text.x = element_text(angle = 45,hjust = 1)) +
   mytheme
-ggsave(filename = paste0(output_path,prefix,"3_DotPlot_allcellfeature.pdf"),plot = plot2,
+ggsave(filename = paste0(output_fpath,prefix,"3_DotPlot_allcellfeature.pdf"),plot = plot2,
        device = "pdf",height = height,width = width*3/2,units = "mm")
 
-#### Stem cell ####
+#### 04_Featurepot of Stem cells ####
 Idents(control.seuobj.singlet) <- control.seuobj.singlet$sum.v2
 StemSubset.3.final <- subset(control.seuobj.singlet,idents = "Precursor cells")
 #StemSubset.3.final <- subset(StemSubset.3.final,
@@ -97,36 +92,27 @@ StemCell_mark <- c("Lin28a","Kit","Ly6a","Flt3","Cd34",
 #StemCell.subset <- subset(StemSubset.3.final,Lin28a == 0)
 plot3 <- FeaturePlot(StemSubset.3.final,features = StemCell_mark,
                      reduction = "umap") + mytheme
-ggsave(filename = paste0(output_path,prefix,"4_FeaturePlot_stemcell_v2.pdf"),plot = plot3,
+ggsave(filename = paste0(output_fpath,prefix,"4_FeaturePlot_stemcell_v2.pdf"),plot = plot3,
        device = "pdf",height = height,width = width*3/2,units = "mm")
 
-#### B cell ####
-Idents(control.seuobj.singlet) <- control.seuobj.singlet$sum.v2
-Bcells <- subset(control.seuobj.singlet,idents = "B cells")
-# Neutrophils.inBcells <- colnames(Bcells)[which(Bcells$singlerCluster == "Neutrophils (GN)")]
-# control.seuobj.singlet$sum.v2[Neutrophils.inBcells] <- "Neutrophils"
-#Idents(Bcells) <- Bcells$singlerCluster
+#### 05_Dimplot of B cells ####
+Idents(Bcells) <- Bcells$singlerCluster
 plot4 <- DimPlot(Bcells,label = T,label.box = T,
                  reduction = "umap",cols = col_cluster,
                  repel = T) + mytheme
-ggsave(filename = paste0(output_path,prefix,"/4_DimPlot_Bcell.pdf"),plot = plot4,
+ggsave(filename = paste0(output_fpath,prefix,"4_DimPlot_Bcell.pdf"),plot = plot4,
        device = "pdf",height = 15,width = 20,units = "cm")
 
-#### Bcell feature ####
+#### 06_Featureplot of B cells ####
 Bcell_features <- c("Cd79a","Ighm","Ebf1",
-                    "H2-DMb2","Ms4a1","Cd74","Ighd",
-                    "Cd72","Vpreb3",
                     "Dntt","Igll1",
-                    "Cd34")
+                    "Vpreb3","Cd72",
+                    "Cd74","Ms4a1","Ighd","H2-DMb2","Igha")
 plot5 <- FeaturePlot(Bcells,features = Bcell_features,reduction = "umap") + mytheme
-ggsave(filename = paste0(output_path,prefix,"5_FeaturePlot_Bcell.pdf"),plot = plot5,
+ggsave(filename = paste0(output_fpath,prefix,"5_FeaturePlot_Bcell.pdf"),plot = plot5,
        device = "pdf",height = height,width = width*3/2,units = "mm")
 
-# Neutrophils.inBcells.v1 <- colnames(Bcells)[which(Bcells$singlerCluster == "B cells (proB.CLP)")]
-# control.seuobj.singlet$sum.v2[Neutrophils.inBcells.v1] <- "Neutrophils"
-# Bcell_subset <- subset(Bcells,tSNE_1 > -2)
-
-#### cell ratio ####
+#### 07_cell proportion between scRNAseq and FACS ####
 orig <- c(rep("scRNAseq",4),rep("FACS",5))
 celltype <- c("Myeloid cells","B cell","T cells","Erythrocytes",
               "Myeloid cells","B cell","T cells","Erythrocytes","Megakaryocyte")
@@ -147,11 +133,11 @@ plot6 <- df.cellratio %>% ggbarplot(x = "Method",
   scale_fill_manual(values = cellfill) +
   mytheme +
   theme_base()
-ggsave(filename = paste0(output_path,prefix,"6_ggbarplot_ratio.pdf"),plot = plot6,
+ggsave(filename = paste0(output_fpath,prefix,"6_ggbarplot_ratio.pdf"),plot = plot6,
        device = "pdf",height = height,width = width*2/3,units = "mm")
 
 
-#### DoHeatmap #### 
+#### 08_Top20 DoHeatmap of all cells #### 
 load("/mnt/data/yjj/BrainImmune/output/data/r3_final.markers.sum.v2.1.RData")
 final.markers.sum.v2.1 %>%
   group_by(cluster) %>%
@@ -160,7 +146,23 @@ Idents(control.seuobj.singlet) <- control.seuobj.singlet$sum.v2
 plot7 <- DoHeatmap(control.seuobj.singlet, features = top20$gene,
                    angle = 90,size = 3,group.colors = anno.final.color) + 
   NoLegend() + mytheme
-ggsave(filename = paste0(output_path,prefix,"7.2_DoHeatmap.pdf"),plot = plot7,
+ggsave(filename = paste0(output_fpath,prefix,"7.2_DoHeatmap.pdf"),plot = plot7,
        device = "pdf",height = height,width = width,units = "mm")
 
+
+
+#### 09_Balloonplot between seurat and RaceID3 ####
+library(gplots)
+library(tidyverse)
+load("./software/RaceID3_StemID2-master/RaceID3_1019/cluster.RData")
+tab.1 <- table(cluster$cpart,cluster$anno)
+pdf(paste0(output_fpath,prefix,"balloonplot.pdf"),
+    width = 12,height = 10)
+balloonplot(tab.1)
+dev.off()
+
+
+#### 10_All RaceID3_StemID2 result ####
+#All of the cold in RaceID3_StemID2_sample_main_sampling_FaceID.R 
+#All of the result in `RaceID3_1019` folder.
 
